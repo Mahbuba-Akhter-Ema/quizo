acd4d5633dadb554a4fb162b3b83cad6f580219f
Mahbuba-Akhter-Ema <mahbubaakhter22@gmail.com>
2022-10-11 02:55:33 +0600

    data fetched

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
-
+   const allQuiz = useLoaderData();
     return (
         <div>
-            
+            <h1>All Quize: {allQuiz.length}</h1>
         </div>
     );
 };
